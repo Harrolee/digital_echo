3fdf4c5ac46a0bcb603afb6dcce5a946cb07b878
Lee Harrold <lhharrold@sep.com>
2023-02-25 16:57:21 -0500

    add recorder #thx mozilla

---
+++ web/recorder.js
@@ -0,0 +1,11 @@
+// set up basic variables for app
+
+const record = document.querySelector(".record");
+const stop = document.querySelector(".stop");
+const soundClips = document.querySelector(".sound-clips");
+const canvas = document.querySelector(".visualizer");
+const mainSection = document.querySelector(".main-controls");
+
+// disable stop button while not recording
+
+stop.disabled = true;
