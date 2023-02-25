aca1b9864670c8882fbdd188ab78d78635101336
Nate White <natewhite1999@gmail.com>
2023-02-25 13:20:32 -0500

    added google cloud to backend

--- packer/src/index.js
+++ packer/src/index.js
@@ -6,6 +6,43 @@ const axios = require("axios");
 
 const app = new koa();
 const router = new koaRouter();
+const fs = require('fs');
+
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+// const filename = 'Local path to audio file, e.g. /path/to/audio.raw';
+// const encoding = 'Encoding of the audio file, e.g. LINEAR16';
+// const sampleRateHertz = 16000;
+// const languageCode = 'BCP-47 language code, e.g. en-US';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false, // If you want interim results, set this to true
+};
+
+// Stream the audio to the Google Cloud Speech API
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data => {
+    console.log(
+      `Transcription: ${data.results[0].alternatives[0].transcript}`
+    );
+  });
+
+// Stream an audio file from disk to the Speech API, e.g. "./resources/audio.raw"
+//fs.createReadStream(filename).pipe(recognizeStream);
 
 render(app, {
     root: path.join(__dirname, "views"),
@@ -14,7 +51,7 @@ render(app, {
 });
 
 router.get("hello", "/", (ctx) => {
-    ctx.body = "<h1>Hello World, Koa folks!</h1>";
+    ctx.body = fs.createReadStream(filename).pipe(recognizeStream);
 });
 
 router.get("users", "/users", async (ctx) => {
