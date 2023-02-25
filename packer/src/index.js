2674b0d183ff0cf6450df069c6cace29405a2fad
Lee Harrold <lhharrold@sep.com>
2023-02-25 18:53:35 -0500

    WIP merge the ends

--- packer/src/index.js
+++ packer/src/index.js
@@ -1,41 +1,43 @@
-
 const koa = require("koa");
 const path = require("path");
 const render = require("koa-ejs");
 const koaRouter = require("koa-router");
 const axios = require("axios");
-const {fromFile} = require("./SpeechRecognition")
+const { fromFile } = require("./SpeechRecognition");
+const { recorder } = require("./components/Recorder");
 const app = new koa();
 const router = new koaRouter();
-const fs = require('fs');
+const fs = require("fs");
 
 render(app, {
-    root: path.join(__dirname, "views"),
-    layout: "index",
-    viewExt: "html",
+  root: path.join(__dirname, "views"),
+  layout: "index",
+  viewExt: "html",
+});
+
+router.get("/", (ctx) => {
+  ctx.body = recorder();
 });
 
 router.post("/", (ctx) => {
-    
-    console.log(ctx.request.body)
-    const wavUrl = audioFile
-    const buffer = Buffer.from(
-        wavUrl.split('base64,')[1],  // only use encoded data after "base64,"
-        'base64'
-    )
-    fs.writeFileSync('./audio.wav', buffer)
-    console.log(`wrote ${buffer.byteLength.toLocaleString()} bytes to file.`)
-    fromFile(audioFile)
+  console.log(ctx.request.body);
+  const wavUrl = audioFile;
+  const buffer = Buffer.from(
+    wavUrl.split("base64,")[1], // only use encoded data after "base64,"
+    "base64"
+  );
+  fs.writeFileSync("./audio.wav", buffer);
+  console.log(`wrote ${buffer.byteLength.toLocaleString()} bytes to file.`);
+  fromFile(audioFile);
 });
 
 router.get("users", "/users", async (ctx) => {
-    const result = await axios.get("https://randomuser.me/api?results=5");
+  const result = await axios.get("https://randomuser.me/api?results=5");
 
-    return ctx.render("index", {
-        users: result.data.results,
-    });
+  return ctx.render("index", {
+    users: result.data.results,
+  });
 });
-
 
 app.use(router.routes()).use(router.allowedMethods());
 
